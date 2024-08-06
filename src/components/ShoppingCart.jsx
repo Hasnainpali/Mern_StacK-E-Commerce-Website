e60a3209f9a869d9184d3871e968659df8763e29
Hasnainpali <hasnainpali321@gmail.com>
2024-08-06 19:23:04 +0500

    checkout url change

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -101,7 +101,7 @@ export default function ShoppingCart() {
         userId: userData.userId
       };
   
-      const response = await fetch(`http://localhost:5000/api/checkout`, {
+      const response = await fetch(`https://full-stack-ecommerce-server.vercel.app/api/checkout`, {
         method:"POST",
         headers:{
           "Content-Type":"application/json"
